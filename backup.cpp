8073788f8665972740b5e5c8dcdd955d717879e1
Bruno <brunoscholz@gmx.de>
2024-11-07 07:15:51 +0100

    Create Backup Class

--- backup.cpp
+++ backup.cpp
@@ -2,51 +2,54 @@
 #include <vector>
 #include <iostream>
 
-int drawBackupMenue();
-void displayBackups();
+class Backup {
+public:
+	void testFunc() {
+		bool running = true;
 
-std::vector<std::vector<std::string>> backups;
-
-void loadBackupMenue(std::vector<std::string> todoList) {
-	bool running = true;
-	system("cls");
-
-	while (running) {
-		switch (drawBackupMenue()) {
-		case 1: 
-			break;
-		case 2: 
-			backups.push_back(todoList);
-			break;
-		case 4:
-			running = false;
-			break;
-		default:
-			break;
+		while (running) {
+			std::cout << "Test" << std::endl;
 		}
 	}
-}
+};
 
-int drawBackupMenue() {
-	system("cls");
 
-	int option = 0;
-	
-	std::cout << "| Backupmenue" << std::endl;
-	std::cout << "|--------------------------------" << std::endl;
-	std::cout << "|  (1) - Backup anzeigen" << std::endl;
-	std::cout << "|  (2) - Backups erstellen" << std::endl;
-	std::cout << "|  (3) - Backup entfernen" << std::endl;
-	std::cout << "|  (4) - Beenden" << std::endl;
-	std::cout << std::endl;
+/*
+int drawBackupMenue();
 
-	std::cin >> option;
+	std::vector<std::vector<std::string>> backups;
 
-	return option;
-}
+	public:
+		void loadBackupMenue(std::vector<std::string> todoList) {
+			bool running = true;
+
+			while (running) {
+				int option = drawBackupMenue();
+				std::cout << option << std::endl;
+			}
+		}
+
+		int drawBackupMenue() {
+			system("cls");
+
+			int option = 0;
+
+			std::cout << "| Backupmenue" << std::endl;
+			std::cout << "|--------------------------------" << std::endl;
+			std::cout << "|  (1) - Backup anzeigen" << std::endl;
+			std::cout << "|  (2) - Backups erstellen" << std::endl;
+			std::cout << "|  (3) - Backup entfernen" << std::endl;
+			std::cout << "|  (4) - Beenden" << std::endl;
+			std::cout << std::endl;
+
+			std::cin >> option;
+
+			return option;
+		}
 
 void displayBackups() {
 	system("cls");
 
 
 }
+*/
